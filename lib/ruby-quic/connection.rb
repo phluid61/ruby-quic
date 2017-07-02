925aa514bd3038036d5e9f58ee640a7cd2f4d0dc
Matthew Kerwin <matthew@kerwin.net.au>
2017-07-02 20:17:25 +1000

    add id, key-writer to connection

--- lib/ruby-quic/connection.rb
+++ lib/ruby-quic/connection.rb
@@ -7,10 +7,12 @@ end
 require_relative 'packet'
 
 class QUIC::Connection
-  def initialize
+  def initialize id
+    @id = id
     @state = 0
     @key = "\xa5\xa5\x0f"
   end
-  attr_reader :state, :key
+  attr_reader :id, :state, :key
+  attr_writer :key
 end
 
